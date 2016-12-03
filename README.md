13dbfc1d45c2dc4119ced673952e039d9fb934e0
Kim Biesbjerg <kb@mindly.dk>
2016-12-03 22:17:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Description
+Use po files with `ng2-translate`
+
+## Usage:
+```ts
+import { Http } from '@angular/http';
+
+import { TranslatePoLoader } from 'ng2-translate-po-loader';
+
+export function createTranslateLoader(http: Http) {
+	return new TranslatePoLoader(http, 'assets/i18n', '.po');
+}
+
+@NgModule({
+	imports: [
+		BrowserModule,
+		HttpModule,
+		TranslateModule.forRoot({
+			provide: TranslateLoader,
+			useFactory: createTranslateLoader,
+			deps: [Http]
+		})
+	],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
+```
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import { TranslateLoader } from 'ng2-translate';
+import * as pofile from 'pofile';
+
+export class TranslatePoLoader implements TranslateLoader {
+
+	constructor(
+		protected _http: Http,
+		protected _prefix: string = 'i18n',
+		protected _suffix: string = '.po'
+	) {
+	}
+
+	/**
+	 * Gets the translations from the server
+	 * @param lang
+	 * @returns {any}
+	 */
+	public getTranslation(lang: string): Observable<any> {
+		return this._http
+			.get(`${this._prefix}/${lang}${this._suffix}`)
+			.map((response: Response) => response.text())
+			.map((data: string) => this._parse(data));
+	}
+
+	/**
+	 * Parse po file
+	 * @param data
+	 * @returns {any}
+	 */
+	protected _parse(data: string): any {
+		let translations = {};
+
+		const parsed = pofile.parse(data);
+		parsed.items.forEach(item => {
+			const id: string = item.msgid;
+			const translation: string = item.msgstr.pop();
+			if (id && translation)  {
+				translations[id] = translation;
+			}
+		});
+
+		return translations;
+	}
+
+}
